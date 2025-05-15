7b2550ffb9189a3968368425725489f1ef80c48a
Lars Nilsson <larsnilsson@mail.com>
2025-05-15 13:01:53 +0200

    Initial commit

---
+++ dronex-manual/oxygen-webhelp/app/search/index/keywords.js
@@ -0,0 +1,17 @@
+var keywords=[{w:"Context",p:["p0"]},{w:"Sensitive",p:["p0"]},{w:"Help",p:["p0"]},{w:"Flight",p:["p1"]},{w:"Modes",p:["p1"]},{w:"Getting",p:["p2"]},{w:"Started",p:["p2"]},{w:"Install",p:["p3","p4"]},{w:"on",p:["p3","p4"]},{w:"Android",p:["p3"]},{w:"iOS",p:["p4"]},{w:"Introduction",p:["p5"]},{w:"Maintenance",p:["p6"]},{w:"Battery",p:["p7"]},{w:"Safety",p:["p7","p9"]},{w:"General",p:["p8"]},{w:"Guidelines",p:["p8"]},{w:"Troubleshooting",p:["p10"]}];
+var ph={};
+ph["p0"]=[0, 1, 2];
+ph["p1"]=[3, 4];
+ph["p2"]=[5, 6];
+ph["p3"]=[7, 8, 9];
+ph["p4"]=[7, 8, 10];
+ph["p5"]=[11];
+ph["p6"]=[12];
+ph["p7"]=[13, 14];
+ph["p8"]=[15, 16];
+ph["p9"]=[14];
+ph["p10"]=[17];
+var keywordsInfo = {
+    keywords: keywords,
+    ph: ph
+}
---
+++ dronex-manual/oxygen-webhelp/app/search/index/link-to-parent.js
@@ -0,0 +1,2 @@
+/*Maps current topic to its parent: "topicIndex:parentIndex". -1 represents the map.*/
+var linkToParent = {5:-1,7:9,1:-1,4:2,8:9,9:-1,3:2,10:-1,2:-1,6:-1};
